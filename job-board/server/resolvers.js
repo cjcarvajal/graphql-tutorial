f426219ca6bf3ce77d71d4af4d3799b751b90fe4
Carlos Carvajal <ing.carlosj@gmail.com>
2023-11-29 11:17:44 -0500

    Replace the hardcoded data to return a real response from the database for Jobs

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,19 +1,7 @@
+import { getJobs } from './db/jobs.js';
+
 export const resolvers = {
     Query: {
-        jobs: () => {
-            return [
-                {
-                    id: 'test-id1',
-                    title: 'Scientific Researcher',
-                    description: 'The scientific researches homolgates a CTO position, with the responsibilities\
-                to define the tecnological vision of the company',
-                },
-                {
-                    id: 'test-id2',
-                    title: 'Chief Finantial Officier',
-                    description: 'In charge of all company budget',
-                }
-            ];
-        }
+        jobs: () => getJobs()
     },
 };
