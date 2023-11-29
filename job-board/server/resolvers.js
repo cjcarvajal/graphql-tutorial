4546cf28a83c204947097bb0b66f8fd719940248
Carlos Carvajal <ing.carlosj@gmail.com>
2023-11-29 10:46:21 -0500

    Add first custom type definition for Job

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,5 +1,12 @@
 export const resolvers = {
     Query: {
-        greeting: () => 'Hello World',
+        job: () => {
+            return {
+                id: 'test-id',
+                title: 'Scientific Researcher',
+                description: 'The scientific researches homolgates a CTO position, with the responsibilities\
+                to define the tecnological vision of the company',
+            }
+        }
     },
 };
